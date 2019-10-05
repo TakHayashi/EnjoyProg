f83a5c4622ba42ff7e2642013733ea4b0eb17372
T.Hayashi <haytak@kyf.biglobe.ne.jp>
2019-10-05 22:22:09 +0900

    stop watch. comment update.

--- sudoku/swatch.py
+++ sudoku/swatch.py
@@ -1,7 +1,7 @@
 #swatch.py
 #by TakHayashi
 #execution time measurement module by closure.
-#usage: see #for test in __main__
+#usage: see test code in __main__ of this.
 from time import perf_counter,time,process_time
 #don't use process_time : resolution > 16ms
 def startwatch():
