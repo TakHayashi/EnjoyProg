8d07a41730281b26feb33f4bbc356e43fd7988e5
T.Hayashi <backtracking@breeze.biglobe.ne.jp>
2020-06-08 18:33:46 +0900

    sudoku problem with 8 solution

---
+++ SudokuSolver/sudq_8solutions.py
@@ -0,0 +1,9 @@
+#sudq_8solutions.py
+#TakHayashi
+#common Sudoku problem for performance comparison between solvers.
+#selected from program generated problems.
+#8 solutions.
+#T.Hayashi
+v=[[8,9,0,0,0,1,0,0,6],[1,6,0,9,0,0,0,0,0],[4,0,0,6,2,0,0,9,0],
+   [0,7,0,0,0,0,6,0,0],[9,0,0,0,0,0,0,3,0],[0,0,1,0,0,0,9,0,7],
+   [5,0,6,7,0,0,0,0,9],[0,0,0,3,5,0,4,0,0],[0,4,0,0,9,0,0,0,2]]
